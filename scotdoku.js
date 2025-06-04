19772e823f063d9e42d02a156094db8f6f24eba2
paravostok <127696302+paravostok@users.noreply.github.com>
2025-06-04 22:39:33 +0100

    Update scotdoku.js

--- scotdoku.js
+++ scotdoku.js
@@ -1,5 +1,5 @@
 // scotdoku.js
-import settlements from './settlements.js';
+import settlements from './settlement.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('📦 Scotdoku script loaded (text‐input mode)');
